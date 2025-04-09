b1532d0d35f42d91e46e0ed9b6f96c9586e57136
YuriLopess <costalopesyuri@gmail.com>
2025-04-09 18:27:11 -0300

    feat: add package models and swap the auto-generated controller for the custom one

---
+++ src/DigitalAccount.API/Controllers/AddCustomerController.cs
@@ -0,0 +1,17 @@
+﻿using DigitalAccount.API.Models.AddCustomer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalAccount.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddCustomerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddCustomer(AddCustomerInput input)
+        {
+            return Created("", input);
+        }
+    }
+}
---
+++ src/DigitalAccount.API/Models/AddCustomer/AddCustomerInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace DigitalAccount.API.Models.AddCustomer
+{
+    public class AddCustomerInput
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+
+
+    }
+}
